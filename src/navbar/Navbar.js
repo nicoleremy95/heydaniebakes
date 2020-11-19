fa4c5c231e9952f753d1406a32034c378bbcb5a0
Nicole Remy <nicoleremy95@gmail.com>
2020-11-18 19:24:43 -0800

    drop downs added and functional

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
     return (
         <div className="Navbar">
             <h1>hey danie bakes</h1>
+            <div className="Navbar-dropdown">
+                <span>RECIPIES</span>
+                    <div className="Navbar-dropdown-content">
+                        <p>RECORD RECIPE</p>
+                        <p>RATE RECIPE</p>
+                    </div>
+            </div>
+            <div className="Navbar-dropdown">
+                <span>TO DOS</span>
+                    <div className="Navbar-dropdown-content">
+                        <p>TO DO E</p>
+                        <p>TO DO</p>
+                    </div>
+            </div>
+            <div className="Navbar-dropdown">
+                <span>SPONSORSHIPS</span>
+                    <div className="Navbar-dropdown-content">
+                        <p>SPONSORSHIPE</p>
+                        <p>SPONSHORSHIP</p>
+                    </div>
+            </div>
         </div>
     )
 }
