fc67f66102f95908eadb3b7ac8bc1f42bde027fa
Nicole Remy <nicoleremy95@gmail.com>
2020-11-27 23:49:37 -0800

    addded star comoponent and started logic

--- src/components/star/Star.js
+++ src/components/star/Star.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Star() {
+    const [rating, setRating] = useState({
+        value,
+        color,
+        handleHover,
+        handleHoverLeave,
+        handleClick,
+        isFilled
+    })
     return (
         <div className="Star">
             
