fe1fc36cf5e4ab70569176a2f059c5acfc19d9e7
Nicole Remy <nicoleremy95@gmail.com>
2020-12-15 21:53:22 -0800

    added ant design, added ant design form, started associating handle changes

--- src/pages/record/Record.js
+++ src/pages/record/Record.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import './Record.css'
+import { Form, Input, Button, Checkbox , InputNumber} from 'antd';
 
 export default function Record() {
     const [formObject, setFormObject] = useState({
@@ -7,27 +9,92 @@ export default function Record() {
         cookTime: "",
         ingredients: "",
         step1: "",
-        cost: "",
-        link: ""
+        step2: "",
+        step3: "",
+        step4: "",
     });
+    
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+    }
+
+    function handleInputChangeSelect(value) {
+        setFormObject({ ...formObject, category: (value) })
+    }
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
+      };
+
     return (
         <div>
             <Navbar/>
             <h1>record recipe</h1>
-            <form>
-            <label for="fname">Name</label>
-            <input type="text" id="name" name="name"></input>
-            <label for="lname">Cook Time</label>
-            <input type="text" id="cook-time" name="cook-time"></input>
-            <label for="lname">Ingredients</label>
-            <input type="text" id="ingredients" name="ingredients"></input>
-            <label for="lname">Step 1</label>
-            <input type="text" id="step-1" name="step-1"></input>
-            <label for="lname">Step 2</label>
-            <input type="text" id="step-2" name="step-2"></input>
-            <label for="lname">Step 3</label>
-            <input type="text" id="step-3" name="step-3"></input>
-            </form>
+            <Form
+                 {...layout}
+                name="basic"
+                initialValues={{ remember: true }}
+                >
+                <Form.Item
+                    name="name"
+                    rules={[{ required: true, message: 'Please input the Name of Recipe!' }]}
+                >
+                    <Input 
+                        placeholder="Name of Recipe"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="cookTime"
+                >
+                    <Input 
+                        placeholder="cookTime"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="ingredients"
+                >
+                    <Input 
+                        placeholder="ingredients"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="step 1"
+                >
+                    <Input.TextArea 
+                        placeholder="step 1"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="step 2"
+                >
+                    <Input.TextArea 
+                        placeholder="step 2"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="step 3"
+                >
+                    <Input.TextArea
+                        placeholder="step 3"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+                <Form.Item
+                    name="step 4"
+                >
+                    <Input.TextArea 
+                        placeholder="step 4"
+                        value={handleInputChange}
+                    />
+                </Form.Item>
+            </Form>
         </div>
     )
 }
